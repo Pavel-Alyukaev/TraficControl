30a56505b65178973ec4da9eb9df060c51c625d9
Pavel-Alyukaev <pavel-alyukaev@yandex.ru>
2017-06-09 18:37:00 +0700

    V001
    Создана форма, подключена EmguCV, подключена камера. Видео выводится на
    форму.

--- TraficControl/Form1.cs
+++ TraficControl/Form1.cs
@@ -8,6 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Cuda;
+using Emgu.CV.Face;
+
+
+
 namespace TraficControl
 {
     public partial class Form1 : Form
@@ -15,6 +24,20 @@ namespace TraficControl
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private VideoCapture video = new VideoCapture();
+
+        private void MoveVideo(object sender, EventArgs e)
+        {
+            
+            IImage image1= video.QueryFrame();
+            imageBox1.Image = image1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.Idle += MoveVideo;
         }
     }
 }
